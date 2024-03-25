4198ccfd2102b19d29f7db058fe86414e940fa0b
Aye10032 <aye10032@gmail.com>
2024-03-25 16:28:07 +0800

    HeatDiagramPlot

--- demo/03plot.py
+++ demo/03plot.py
@@ -157,3 +157,25 @@ class PolygonOnAxes(Scene):
         self.play(t.animate.set_value(10))
         self.play(t.animate.set_value(k / 10))
         self.play(t.animate.set_value(5))
+
+
+class HeatDiagramPlot(Scene):
+    def construct(self):
+        ax = Axes(
+            x_range=[0, 40, 5],
+            y_range=[-8, 32, 5],
+            x_length=9,
+            y_length=6,
+            x_axis_config={'numbers_to_include': np.arange(0, 40, 5)},
+            y_axis_config={'numbers_to_include': np.arange(-5, 34, 5)},
+        )
+        labels = ax.get_axis_labels(
+            x_label=Tex("$\\Delta Q$"),
+            y_label=Tex("T[$^\\circ C$]")
+        )
+
+        x_vals = [0, 8, 30, 39]
+        y_vals = [20, 0, 0, -6]
+        graph = ax.plot_line_graph(x_values=x_vals, y_values=y_vals)
+
+        self.add(ax, labels, graph)
