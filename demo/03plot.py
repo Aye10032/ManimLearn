5bb669660399f05a97827147c1fee69f46250aec
Aye10032 <aye10032@gmail.com>
2024-03-25 16:17:58 +0800

    PolygonOnAxes

--- demo/03plot.py
+++ demo/03plot.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from manim import *
 import numpy as np
 
@@ -96,3 +98,62 @@ class GraphArePlot(Scene):
         area2 = ax.get_area(curve2, (2, 3), bounded_graph=curve1, color=GRAY, opacity=0.5)
 
         self.add(area1, area2)
+
+
+class PolygonOnAxes(Scene):
+
+    def get_rectangle_corners(self, bottom_left: Tuple[float, float], top_right: Tuple[float, float]):
+        return [
+            (top_right[0], top_right[1]),
+            (bottom_left[0], top_right[1]),
+            (bottom_left[0], bottom_left[1]),
+            (top_right[0], bottom_left[1])
+        ]
+
+    def construct(self):
+        ax = Axes(
+            x_range=[0, 10],
+            y_range=[0, 10],
+            x_length=6,
+            y_length=6,
+            tips=True
+        )
+
+        t = ValueTracker(5)
+        k = 25
+
+        graph = ax.plot(
+            lambda x: k / x,
+            color=YELLOW_D,
+            x_range=[k / 10, 10.0, 0.01],
+            use_smoothing=False
+        )
+
+        def get_rectangle() -> VMobject:
+            _polygon = Polygon(*[
+                ax.c2p(*i)
+                for i in self.get_rectangle_corners(
+                    (0, 0), (t.get_value(), k / t.get_value())
+                )
+            ])
+            _polygon.stroke_width = 1
+            _polygon.set_fill(BLUE, opacity=0.5)
+            _polygon.set_stroke(YELLOW_B)
+
+            return _polygon
+
+        polygon = always_redraw(get_rectangle)
+
+        def dot_updater(mobj: Mobject):
+            mobj.move_to(ax.c2p(t.get_value(), k / t.get_value()))
+
+        dot = Dot()
+        dot.add_updater(dot_updater)
+        dot.set_z_index(10)
+
+        self.add(ax, graph)
+        self.play(FadeIn(dot))
+        self.play(Create(polygon))
+        self.play(t.animate.set_value(10))
+        self.play(t.animate.set_value(k / 10))
+        self.play(t.animate.set_value(5))
