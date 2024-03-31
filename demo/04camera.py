372b70ddcad78799c07c10bc5f124d42d7f00efc
Aye10032 <aye10032@gmail.com>
2024-03-31 17:01:52 +0800

    ThreeDCameraIllusionRotation

--- demo/04camera.py
+++ demo/04camera.py
@@ -142,3 +142,15 @@ class ThreeDCameraRotation(ThreeDScene):
         self.stop_ambient_camera_rotation()
         self.move_camera(phi=75 * DEGREES, theta=30 * DEGREES)
         self.wait()
+
+
+class ThreeDCameraIllusionRotation(ThreeDScene):
+    def construct(self):
+        ax = ThreeDAxes()
+        circle = Circle()
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.add(circle, ax)
+
+        self.begin_3dillusion_camera_rotation(rate=2)
+        self.wait()
+        self.stop_3dillusion_camera_rotation()
