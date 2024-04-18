1cf870f89b1451d190ac6757d736f5191d13617e
Aye10032 <aye10032@gmail.com>
2024-04-18 16:49:56 +0800

    update demo

--- demo/01basic.py
+++ demo/01basic.py
@@ -25,18 +25,19 @@ class ManimCELogo(Scene):
 
 class BraceAnnotation(Scene):
     def construct(self):
-        dot1 = Dot(np.array([-2, -1, 0]))
-        dot2 = Dot(np.array([2, 1, 0]))
+        dot1 = Dot(np.array([0, 0, 0]))
+        dot2 = Dot(np.array([4, 2, 0]))
+        dot2_tex = MathTex(r'(4, 2)', color=BLUE).next_to(dot2, RIGHT)
+
+        ax = Axes()
         line = Line(dot1.get_center(), dot2.get_center()).set_color(ORANGE)
+        b1 = Brace(line).set_color(TEAL)
+        b1_text = b1.get_text('Horizontal distance').set_color(YELLOW)
 
-        b1 = Brace(line)
-        b1_text = b1.get_text('Horizontal distance')
-
-        # b2 = BraceBetweenPoints([-2, -1, 0], [2, 1, 0], direction=line.copy().rotate(PI/2).get_unit_vector())
-        b2 = Brace(line, direction=line.copy().rotate(PI / 2).get_unit_vector())
+        b2 = Brace(line, direction=line.copy().rotate(PI / 2).get_unit_vector()).set_color(TEAL)
         b2_text = b2.get_tex(r'x-x_1')
 
-        self.add(line, dot1, dot2, b1, b1_text, b2, b2_text)
+        self.add(ax, line, dot1, dot2, dot2_tex, b1, b1_text, b2, b2_text)
 
 
 class VectorArrow(Scene):
